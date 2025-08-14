e1ad5b2a1e1bcb66eea790cd06f53dc1955822b0
AlWalletTracker <jayaprakashinzoom@gmail.com>
2025-08-14 18:20:46 +0530

    Delete GanatheExpTracker_PWA_stable directory

--- GanatheExpTracker_PWA_stable/service-worker.js
+++
@@ -1,9 +0,0 @@
-const CACHE_NAME = 'ganathe-pwa-v1';
-const FILES_TO_CACHE = ['/', '/index.html', '/README.md'];
-self.addEventListener('install', (evt) => {
-  evt.waitUntil(caches.open(CACHE_NAME).then((cache)=>cache.addAll(FILES_TO_CACHE)));
-  self.skipWaiting();
-});
-self.addEventListener('fetch', (evt)=>{
-  evt.respondWith(caches.match(evt.request).then((r)=>r || fetch(evt.request)));
-});
